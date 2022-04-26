5669dfb7cb9f99cfe460daafc604596c084c1474
White <whitemusicofc@gmail.com>
2022-04-26 17:53:41 -0300

    novos produtos na loja, trabalhando ainda no css/js

--- js/loja.js
+++ js/loja.js
@@ -1,6 +1,19 @@
 const item1 = document.querySelector('#card1-hover');
 const item2 = document.querySelector('#card2-hover');
 const item3 = document.querySelector('#card3-hover');
+const item4 = document.querySelector('#card4-hover');
+const item5 = document.querySelector('#card5-hover');
+const item6 = document.querySelector('#card6-hover');
+const item7 = document.querySelector('#card7-hover');
+const item8 = document.querySelector('#card8-hover');
+
+
+
+produtos = [item1,item2,item3,item4,item5,item6,item7,item8]
+
+console.log(produtos[2]);
+
+
 
 // const cards = {
 //     item1 = document.querySelector(#)
@@ -12,108 +25,41 @@ const item3 = document.querySelector('#card3-hover');
 // for(i = 0; i < 8; i++){    
 // }
 
-item1.addEventListener('mouseover', () =>{
+// item1.addEventListener('mouseover', () =>{
 
-    if(item1.src.includes('capa_iphone_white_branca.png')){
-        item1.src = '../imgs/loja/capa_iphone_daleinsano_azul.png';
-    }
-});
+//     if(item1.src.includes('capa_iphone_white_branca.png')){
+//         item1.src = '../imgs/loja/capa_iphone_daleinsano_azul.png';
+//     }
+// });
 
-item1.addEventListener('mouseout', () =>{
-    if(!item1.src.includes('capa_iphone_white_branca.png')){
-        item1.src = '../imgs/loja/capa_iphone_white_branca.png';
-    }
-});
+// item1.addEventListener('mouseout', () =>{
+//     if(!item1.src.includes('capa_iphone_white_branca.png')){
+//         item1.src = '../imgs/loja/capa_iphone_white_branca.png';
+//     }
+// });
 
-item2.addEventListener('mouseover', () =>{
+// item2.addEventListener('mouseover', () =>{
 
-    if(item2.src.includes('bone_white_vermelho_frente.png')){
-        item2.src = '../imgs/loja/bone_white_daleinsano_tras.png';
-    }
-});
+//     if(item2.src.includes('bone_white_vermelho_frente.png')){
+//         item2.src = '../imgs/loja/bone_white_daleinsano_tras.png';
+//     }
+// });
 
-item2.addEventListener('mouseout', () =>{
-    if(!item2.src.includes('bone_white_vermelho_frente.png')){
-        item2.src = '../imgs/loja/bone_white_vermelho_frente.png';
-    }
-});
+// item2.addEventListener('mouseout', () =>{
+//     if(!item2.src.includes('bone_white_vermelho_frente.png')){
+//         item2.src = '../imgs/loja/bone_white_vermelho_frente.png';
+//     }
+// });
 
-item3.addEventListener('mouseover', () =>{
+// item3.addEventListener('mouseover', () =>{
 
-    if(item3.src.includes('bone_white_daleinsano_azul_frente.png')){
-        item3.src = '../imgs/loja/bone_white_dale_insano_tras.png';
-    }
-});
+//     if(item3.src.includes('bone_white_daleinsano_azul_frente.png')){
+//         item3.src = '../imgs/loja/bone_white_dale_insano_tras.png';
+//     }
+// });
 
-item3.addEventListener('mouseout', () =>{
-    if(!item3.src.includes('bone_white_daleinsano_azul_frente.png')){
-        item3.src = '../imgs/loja/bone_white_daleinsano_azul_frente.png';
-    }
-});
-
-
-
-item4.addEventListener('mouseover', () =>{
-
-    if(item1.src.includes('capa_iphone_white.png')){
-        item1.src = '../imgs/loja/capa_iphone_daleinsano_azul.png';
-    }
-});
-
-item4.addEventListener('mouseout', () =>{
-    if(!item1.src.includes('capa_iphone_white.png')){
-        item1.src = '../imgs/loja/capa_iphone_white.png';
-    }
-});
-
-item5.addEventListener('mouseover', () =>{
-
-    if(item1.src.includes('capa_iphone_white.png')){
-        item1.src = '../imgs/loja/capa_iphone_daleinsano_azul.png';
-    }
-});
-
-item5.addEventListener('mouseout', () =>{
-    if(!item1.src.includes('capa_iphone_white.png')){
-        item1.src = '../imgs/loja/capa_iphone_white.png';
-    }
-});
-
-item6.addEventListener('mouseover', () =>{
-
-    if(item1.src.includes('capa_iphone_white.png')){
-        item1.src = '../imgs/loja/capa_iphone_daleinsano_azul.png';
-    }
-});
-
-item6.addEventListener('mouseout', () =>{
-    if(!item1.src.includes('capa_iphone_white.png')){
-        item1.src = '../imgs/loja/capa_iphone_white.png';
-    }
-});
-
-item7.addEventListener('mouseover', () =>{
-
-    if(item1.src.includes('capa_iphone_white.png')){
-        item1.src = '../imgs/loja/capa_iphone_daleinsano_azul.png';
-    }
-});
-
-item7.addEventListener('mouseout', () =>{
-    if(!item1.src.includes('capa_iphone_white.png')){
-        item1.src = '../imgs/loja/capa_iphone_white.png';
-    }
-});
-
-item8.addEventListener('mouseover', () =>{
-
-    if(item1.src.includes('capa_iphone_white.png')){
-        item1.src = '../imgs/loja/capa_iphone_daleinsano_azul.png';
-    }
-});
-
-item8.addEventListener('mouseout', () =>{
-    if(!item1.src.includes('capa_iphone_white.png')){
-        item1.src = '../imgs/loja/capa_iphone_white.png';
-    }
-});
+// item3.addEventListener('mouseout', () =>{
+//     if(!item3.src.includes('bone_white_daleinsano_azul_frente.png')){
+//         item3.src = '../imgs/loja/bone_white_daleinsano_azul_frente.png';
+//     }
+// });
